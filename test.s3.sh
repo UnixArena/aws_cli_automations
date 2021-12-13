9e13e100d47e2108dd102ba55b1d300c4215c9c1
Lingeswaran R <lingeshwaran.rangasamy@gmail.com>
2021-12-13 08:02:44 +0530

    Create test.s3.sh

---
+++ test.s3.sh
@@ -0,0 +1,9 @@
+   while read -r line; do
+   # Determine source s3 buckets are readable
+   read_api_exit=0
+   for  file_name in `aws s3api list-objects-v2 --bucket $line | grep key | awk ' { print $2 } ' | tr -d \",\,`; do aws s3api get-object-acl --bucket $line --key $file_name >/dev/null done  || read_api_exit=$? ; done
+   #########aws s3api list-objects-v2 --bucket $line > /dev/null || read_api_exit=$?
+    if [[ $read_api_exit -eq 0 ]]; then
+        LASTM=`aws s3 ls "$line" --recursive | sort -r |awk ' { print $1 } '  | head -1`
+        if [[ -n "$LASTM" ]] && [[ "$LASTM" < "$SINCE" ]]; then
+              copy_api_exit=0
