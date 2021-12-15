728d1a24c0fc399230121f5e4ab654f4b660c3ac
Lingeswaran R <lingeshwaran.rangasamy@gmail.com>
2021-12-15 20:55:52 +0530

    Update unused_ebs_volume.sh

--- unused_ebs_volume.sh
+++ unused_ebs_volume.sh
@@ -10,3 +10,5 @@ aws ec2 describe-volumes  --filters Name=status,Values=available --query "Volume
 aws ec2 describe-volumes --filters Name=status,Values=available Name=tag-key,Values=LastDetachTime --query "Volumes[*].{VolumeID:VolumeId,Region:AvailabilityZone,State:State,CreationTime:CreateTime}" --output text | tr -d [,{,\",\},\],\' > Tag_Volumes_LastDetachTime
 
 awk 'NR==FNR{a[$0];next}(!($0 in a)){print}' Tag_Volumes_LastDetachTime EBS_Volume_List_Last_60days > EBS_Unsed_Volume_without_LastDetachTime
+
+aws cloudtrail lookup-events --lookup-attributes AttributeKey=ResourceName,AttributeValue=$VOLID --max-results 3 --region us-east-2c --query 'Events[?EventName == `DetachVolume`].CloudTrailEvent'  | sed 's/\\//g' | sed 's/"}"/"}/g' | sed 's/"{"/{"/g' | awk -F"," '{print $11}' | tr -d \",Z,eventTime: | cut -c 1-10
